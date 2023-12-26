6b529ae33751c6802bb32b499b3270dd112ff485
BirdBird <birdbirdmurmur@gmail.com>
2023-12-26 16:48:44 +0800

    Implemented Outlet in Root.jsx for main content switching

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,13 +1,15 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import { Types } from "../routes/Types"
+import { Outlet } from "react-router-dom"
 
 const Root = () => {
 
   return (
     <>
       <Header />
-      <Types />
+      {/* <Types /> */}
+      <Outlet />
       <Footer />
     </>
   )
