1e508038e25f31d8d5992a96fc296e307e62fa15
BirdBird <birdbirdmurmur@gmail.com>
2023-12-27 15:36:51 +0800

    Fetch and Display Pokémon (1st Gen)

--- src/routes/Pokedex.jsx
+++ src/routes/Pokedex.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+import Container from '@mui/material/Container'
+
+const Pokedex_URL = 'https://pokemon-go-api.github.io/pokemon-go-api/api/pokedex.json'
 
 export const Pokedex = () => {
+    const [pokedex, setPokedex] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios.get(Pokedex_URL)
+            setPokedex(result.data)
+            console.log(result.data)
+        }
+
+        fetchData()
+    }, [])
+
     return (
-        <div>Pokedex</div>
+        <>
+            <Container maxWidth="lg" sx={{ height: '88vh', overflowY: 'scroll' }} >
+                {pokedex.map((item, index) => {
+                    if (item.generation === 1) {
+                        return (
+                            <div key={index}>
+                                <h3>{item.dexNr}. {item.names.English}</h3>
+                                {/* IMG cannot be loaded */}
+                                {/* {item.assets.image && <img src={item.assets.image} alt={item.names.English} />} */}
+                                <ul>
+                                    <li>{item.primaryType.names.English}</li>
+                                    {item.secondaryType && <li>{item.secondaryType.names.English}</li>}
+                                </ul>
+                            </div>
+                        )
+                    }
+                })}
+            </Container>
+        </>
     )
 }
