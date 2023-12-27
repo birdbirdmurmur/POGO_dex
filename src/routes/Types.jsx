f75073d4d33026f0c22a56709917b5bb038041ca
BirdBird <birdbirdmurmur@gmail.com>
2023-12-27 15:50:58 +0800

    Improve UI layout

--- src/routes/Types.jsx
+++ src/routes/Types.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Grid from '@mui/material/Grid'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Divider from '@mui/material/Divider'
 
 const URL = 'https://pokemon-go-api.github.io/pokemon-go-api/api/types.json'
 
@@ -9,9 +14,12 @@ export const Types = () => {
 
     useEffect(() => {
         const fetchData = async () => {
-            const result = await axios.get(URL)
-            setData(result.data)
-            console.log(result.data)
+            try {
+                const response = await axios.get(URL)
+                setData(response.data)
+            } catch (error) {
+                console.log('Error fetching data:', error)
+            }
         }
 
         fetchData()
@@ -19,16 +27,35 @@ export const Types = () => {
 
     return (
         <>
-            <Container maxWidth="lg" sx={{ py: 8, border: '1px solid red', height: '88vh', overflowY: 'scroll' }} >
-                <h1>Types</h1>
-                {data.map((item, index) => (
-                    <div key={index}>
-                        <h3>主動方：{item.type}</h3>
-                        <p>打誰傷害減半： {item.halfDamageFrom}</p>
-                        <p>打誰無效： {item.noDamageFrom}</p>
-                        <p>被誰剋： {item.doubleDamageFrom}</p>
-                    </div>
-                ))}
+            <Container maxWidth="lg" sx={{ height: '88vh', overflowY: 'scroll' }} >
+                <Typography variant="h3" gutterBottom align="center">
+                    Types
+                </Typography>
+                <Grid container spacing={3}>
+                    {data.map((item, index) => (
+                        <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+                            <Card sx={{ height: '100%' }}>
+                                <CardContent>
+                                    <Typography variant="h5" color="primary" gutterBottom>
+                                        主動方：{item.type}
+                                    </Typography>
+                                    <Divider />
+                                    <Typography variant="subtitle1" gutterBottom>
+                                        打誰傷害減半： {item.halfDamageFrom.join(', ')}
+                                    </Typography>
+                                    <Divider />
+                                    <Typography variant="subtitle1" gutterBottom>
+                                        打誰無效： {item.noDamageFrom.join(', ')}
+                                    </Typography>
+                                    <Divider />
+                                    <Typography variant="subtitle1" gutterBottom>
+                                        被誰剋： {item.doubleDamageFrom.join(', ')}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
             </Container>
         </>
     )
