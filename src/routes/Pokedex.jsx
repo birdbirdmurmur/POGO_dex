60f6e9cdf7b118f3734988d8ef465661b0ffacf2
BirdBird <birdbirdmurmur@gmail.com>
2023-12-27 16:16:10 +0800

    Improve Pokedex UI layout

--- src/routes/Pokedex.jsx
+++ src/routes/Pokedex.jsx
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Chip from '@mui/material/Chip'
+import Typography from '@mui/material/Typography'
 
 const Pokedex_URL = 'https://pokemon-go-api.github.io/pokemon-go-api/api/pokedex.json'
 
@@ -10,33 +15,44 @@ export const Pokedex = () => {
 
     useEffect(() => {
         const fetchData = async () => {
-            const result = await axios.get(Pokedex_URL)
-            setPokedex(result.data)
-            console.log(result.data)
+            try {
+                const response = await axios.get(Pokedex_URL)
+                setPokedex(response.data)
+            } catch (error) {
+                console.log('Error fetching data:', error)
+            }
         }
 
         fetchData()
     }, [])
 
     return (
-        <>
-            <Container maxWidth="lg" sx={{ height: '88vh', overflowY: 'scroll' }} >
+        <Container maxWidth="lg" sx={{ height: '88vh', overflowY: 'scroll', pt: '20px' }} >
+            <Grid container spacing={2}>
                 {pokedex.map((item, index) => {
                     if (item.generation === 1) {
                         return (
-                            <div key={index}>
-                                <h3>{item.dexNr}. {item.names.English}</h3>
-                                {/* IMG cannot be loaded */}
-                                {/* {item.assets.image && <img src={item.assets.image} alt={item.names.English} />} */}
-                                <ul>
-                                    <li>{item.primaryType.names.English}</li>
-                                    {item.secondaryType && <li>{item.secondaryType.names.English}</li>}
-                                </ul>
-                            </div>
+                            <Grid item xs={6} sm={4} md={3} key={index}>
+                                <Card>
+                                    <CardContent>
+                                        <Typography>
+                                            {item.dexNr}. {item.names.English}
+                                        </Typography>
+                                        {/* IMG cannot be loaded */}
+                                        {/* {item.assets.image && <img src={item.assets.image} alt={item.names.English} />} */}
+                                        <div>
+                                            <Chip label={item.primaryType.names.English} />
+                                            {item.secondaryType && (
+                                                <Chip label={item.secondaryType.names.English} />
+                                            )}
+                                        </div>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
                         )
                     }
                 })}
-            </Container>
-        </>
+            </Grid>
+        </Container>
     )
 }
