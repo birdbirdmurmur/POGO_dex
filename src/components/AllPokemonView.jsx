9817d2c01548b129ddd9df24cd53888eb54c74ff
BirdBird <birdbirdmurmur@gmail.com>
2023-12-31 16:42:59 +0800

    Separated AllPokemonView in to a JSX component

---
+++ src/components/AllPokemonView.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import Grid from '@mui/material/Grid'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+import Chip from '@mui/material/Chip'
+import CardMedia from '@mui/material/CardMedia'
+
+export const AllPokemonView = ({ item, getZhType, getBgColor }) => {
+    return (
+        <Grid item xs={12} sm={6} md={4}>
+            <Card sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                height: '100%',
+            }}>
+                <CardContent>
+                    <Typography variant="h6" sx={{ mb: 1 }}>
+                        {item.dexNr}. {item.names.English}
+                    </Typography>
+                    <Chip
+                        label={getZhType(item.primaryType.names.English)}
+                        sx={{ backgroundColor: getBgColor(item.primaryType.names.English) }}
+                    />
+                    {item.secondaryType && (
+                        <Chip
+                            label={getZhType(item.secondaryType.names.English)}
+                            sx={{ backgroundColor: getBgColor(item.secondaryType.names.English) }}
+                        />
+                    )}
+                </CardContent>
+                {/* some pokemon don't have an image */}
+                {item.assets && item.assets.image ? (
+                    <CardMedia
+                        component="img"
+                        image={item.assets.image}
+                        alt={item.names.English}
+                        sx={{
+                            width: '80px',
+                            height: '80px',
+                            objectFit: 'contain',
+                            marginRight: '10px',
+                        }}
+                    />
+                ) : (
+                    <CardMedia
+                        component="img"
+                        image='https://via.placeholder.com/80x80'
+                        alt={item.names.English}
+                        sx={{
+                            width: '80px',
+                            height: '80px',
+                            objectFit: 'contain',
+                            marginRight: '10px',
+                        }}
+                    />
+                )}
+            </Card>
+        </Grid>
+    )
+}
--- src/routes/Pokedex.jsx
+++ src/routes/Pokedex.jsx
@@ -2,15 +2,11 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 import Grid from '@mui/material/Grid'
-import Card from '@mui/material/Card'
-import CardContent from '@mui/material/CardContent'
-import Chip from '@mui/material/Chip'
-import Typography from '@mui/material/Typography'
-import { CardMedia } from '@mui/material'
 
 import { Heading } from '../components/Heading'
 import { SearchBar } from '../components/SearchBar'
 import { FilterButtons } from '../components/FilterButtons'
+import { AllPokemonView } from '../components/AllPokemonView'
 import { allTypes } from '../data'
 
 const Pokedex_URL = 'https://pokemon-go-api.github.io/pokemon-go-api/api/pokedex.json'
@@ -57,48 +53,16 @@ export const Pokedex = () => {
             {/* All Data */}
             <Grid container spacing={2}>
                 {filteredPokedex.map((item, index) => {
-                    if (item.generation === 1) {
-                        return (
-                            <Grid item xs={12} sm={6} md={4} key={index}>
-                                <Card sx={{
-                                    display: 'flex',
-                                    justifyContent: 'space-between',
-                                    alignItems: 'center',
-                                    height: '100%',
-                                }}>
-                                    <CardContent>
-                                        <Typography variant="h6" sx={{ mb: 1 }}>
-                                            {item.dexNr}. {item.names.English}
-                                        </Typography>
-                                        <Chip
-                                            label={getZhType(item.primaryType.names.English)}
-                                            sx={{ backgroundColor: getBgColor(item.primaryType.names.English) }}
-                                        />
-                                        {item.secondaryType && (
-                                            <Chip
-                                                label={getZhType(item.secondaryType.names.English)}
-                                                sx={{ backgroundColor: getBgColor(item.secondaryType.names.English) }}
-                                            />
-                                        )}
-                                    </CardContent>
-                                    {/* IMG cannot be loaded in Gen6 */}
-                                    {item.assets.image &&
-                                        <CardMedia
-                                            component="img"
-                                            image={item.assets.image}
-                                            alt={item.names.English}
-                                            sx={{
-                                                width: '80px',
-                                                height: '80px',
-                                                objectFit: 'contain',
-                                                marginRight: '10px',
-                                            }}
-                                        />
-                                    }
-                                </Card>
-                            </Grid>
-                        )
-                    }
+                    // if (item.generation === 1) {
+                    return (
+                        <AllPokemonView
+                            key={index}
+                            item={item}
+                            getZhType={getZhType}
+                            getBgColor={getBgColor}
+                        />
+                    )
+                    // }
                 })}
             </Grid>
         </React.Fragment>
